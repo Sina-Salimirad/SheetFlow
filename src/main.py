f08062e9a7477e17ac5e678a821d50c418ae6b15
Daniii83 <dani.salmanzade@gmail.com>
2025-08-29 19:15:34 +0200

    Open exel file (#2)
    * new update
    
    * see data

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
+import pandas as pd
+
 from __version__ import __version__
 
 if __name__ == "__main__":
   print("project version:", __version__)
+
+read = pd.read_excel("input.xlsx")
+print(read)
+
+column = "Name"
+if column in read:
+  print(read[["Name","Age"]])
